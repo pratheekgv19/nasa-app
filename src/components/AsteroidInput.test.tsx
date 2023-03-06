1427feae45791cbb19adf319aa30f105bb9422ee
Pratheek G V <109038768+pratheek1904@users.noreply.github.com>
2023-03-06 10:33:30 +0530

    feature file added

--- src/components/AsteroidInput.test.tsx
+++ src/components/AsteroidInput.test.tsx
@@ -1,15 +1,9 @@
 import { shallow } from "enzyme";
 import AsteroidInput from "./AsteroidInput";
+import {render, fireEvent, screen } from "@testing-library/react";
 
-describe("tets cases",()=>{
-
-})
 
 describe("AsteroidInput", () => {
-
-  // beforeEach(() => {
-  //  const wrapper = shallow(<AsteroidInput posts={[]} />);
-  // });
 
   const wrapper = shallow(<AsteroidInput posts={[]} />);
   it("should render a form with an input and a submit button", () => {
@@ -45,6 +39,65 @@ describe("AsteroidInput", () => {
     expect(details.length).toBe(0);
   });
 });
+
+describe("asteroid details after entering an id and submitting",()=>{
+  test("should show the asteroid details after submitting with a valid ID", async () => {
+    const mockData = {
+      name: "Mock Asteroid",
+      nasa_jpl_url: "https://www.nasa.gov",
+      is_potentially_hazardous_asteroid: true,
+    };
+    global.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockData),
+      })
+    );
+    render(<AsteroidInput posts={[]} />);
+    const input = screen.getByTestId("input");
+    const submitButton = screen.getByTestId("submit");
+    fireEvent.change(input, { target: { value: "1234" } });
+    fireEvent.click(submitButton);
+    expect(await screen.findByText("Asteroid Details:")).toBeInTheDocument();
+    expect(screen.getByTestId("name")).toHaveTextContent("Mock Asteroid");
+    expect(screen.getByTestId("url")).toHaveTextContent("https://www.nasa.gov");
+    expect(screen.getByTestId("isHazardous")).toHaveTextContent("Is Potentially Hazardous Asteroid:Yes");
+  });
+})
+
+describe("random Asteroid",()=>{
+
+it("should show a random asteroid details after clicking the button", async () => {
+  const mockData = {
+    name: "Mock Random Asteroid",
+    nasa_jpl_url: "https://www.nasa.gov",
+    is_potentially_hazardous_asteroid: false,
+  };
+  global.fetch = jest.fn().mockImplementation((url) => {
+    if (url.includes("browse")) {
+      return Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            near_earth_objects: [{ id: "5678" }, { id: "9012" }],
+          }),
+      });
+    } else {
+      return Promise.resolve({
+        json: () => Promise.resolve(mockData),
+      });
+    }
+  });
+
+  render(<AsteroidInput posts={[]} />);
+  const randomButton = screen.getByText("Random Asteroid");
+  fireEvent.click(randomButton);
+  expect(await screen.findByText("Asteroid Details:")).toBeInTheDocument();
+  expect(screen.getByTestId("name")).toHaveTextContent("Mock Random Asteroid");
+  expect(screen.getByTestId("url")).toHaveTextContent("https://www.nasa.gov");
+  expect(screen.getByTestId("isHazardous")).toHaveTextContent(
+    "Is Potentially Hazardous Asteroid:No"
+  );
+})
+})
 
 
 
@@ -256,85 +309,5 @@ describe("AsteroidInput", () => {
 //       });
 // })
 
-  // describe("asteroid details after entering an id and submitting",()=>{
-  //   test("should show the asteroid details after submitting with a valid ID", async () => {
-  //     const mockData = {
-  //       name: "Mock Asteroid",
-  //       nasa_jpl_url: "https://www.nasa.gov",
-  //       is_potentially_hazardous_asteroid: true,
-  //     };
-  //     global.fetch = jest.fn().mockImplementation(() =>
-  //       Promise.resolve({
-  //         json: () => Promise.resolve(mockData),
-  //       })
-  //     );
-  //     render(<AsteroidInput posts={[]} />);
-  //     const input = screen.getByTestId("input");
-  //     const submitButton = screen.getByTestId("submit");
-  //     fireEvent.change(input, { target: { value: "1234" } });
-  //     fireEvent.click(submitButton);
-  //     expect(await screen.findByText("Asteroid Details:")).toBeInTheDocument();
-  //     expect(screen.getByTestId("name")).toHaveTextContent("Mock Asteroid");
-  //     expect(screen.getByTestId("url")).toHaveTextContent("https://www.nasa.gov");
-  //     expect(screen.getByTestId("isHazardous")).toHaveTextContent("Is Potentially Hazardous Asteroid:Yes");
-  //   });
-  // })
-  
-// describe("random Asteroid",()=>{
-  
-//   it("should show a random asteroid details after clicking the button", async () => {
-//     const mockData = {
-//       name: "Mock Random Asteroid",
-//       nasa_jpl_url: "https://www.nasa.gov",
-//       is_potentially_hazardous_asteroid: false,
-//     };
-//     global.fetch = jest.fn().mockImplementation((url) => {
-//       if (url.includes("browse")) {
-//         return Promise.resolve({
-//           json: () =>
-//             Promise.resolve({
-//               near_earth_objects: [{ id: "5678" }, { id: "9012" }],
-//             }),
-//         });
-//       } else {
-//         return Promise.resolve({
-//           json: () => Promise.resolve(mockData),
-//         });
-//       }
-//     });
 
-//     render(<AsteroidInput posts={[]} />);
-//     const randomButton = screen.getByText("Random Asteroid");
-//     fireEvent.click(randomButton);
-//     expect(await screen.findByText("Asteroid Details:")).toBeInTheDocument();
-//     expect(screen.getByTestId("name")).toHaveTextContent("Mock Random Asteroid");
-//     expect(screen.getByTestId("url")).toHaveTextContent("https://www.nasa.gov");
-//     expect(screen.getByTestId("isHazardous")).toHaveTextContent(
-//       "Is Potentially Hazardous Asteroid:No"
-//     );
-// })
-// })
-// describe("asteroid details after entering an id and submitting", () => {
-//   test("should show the asteroid details after submitting with a valid ID", async () => {
-//     const mockData = {
-//       name: "Mock Asteroid",
-//       nasa_jpl_url: "https://www.nasa.gov",
-//       is_potentially_hazardous_asteroid: true,
-//     };
-//     global.fetch = jest.fn().mockResolvedValue({
-//       json: () => Promise.resolve(mockData),
-//     });
-//     render(<AsteroidInput posts={[]} />);
-//     const input = screen.getByTestId("input");
-//     const submitButton = screen.getByTestId("submit");
-//     fireEvent.change(input, { target: { value: "1234" } });
-//     fireEvent.click(submitButton);
-//     expect(await screen.findByText("Asteroid Details:")).toBeInTheDocument();
-//     expect(screen.getByTestId("name")).toHaveTextContent("Mock Asteroid");
-//     expect(screen.getByTestId("url")).toHaveTextContent("https://www.nasa.gov");
-//     expect(screen.getByTestId("isHazardous")).toHaveTextContent(
-//       "Is Potentially Hazardous Asteroid:Yes"
-//     );
-//   });
-// });
 
